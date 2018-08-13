74856df8e78a8cb13151685f0d26348bd10845a5
Thadishi <thsdishi@gmail.com>
2018-08-14 01:17:47 +0200

    homepage kind of works

--- mapping/views.py
+++ mapping/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User, Nests, DEM
+
+def index(request):
+
+	num_nests = Nests.objects.all().count()
+
+	context = {
+		'num_nests': num_nests
+	}
+
+	return render(request, 'index.html', context=context)
