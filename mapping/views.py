a4e1cfb29470120b2062c63d39532d1f85d2a908
Thadishi <thsdishi@gmail.com>
2018-08-14 15:49:45 +0200

    map render

--- mapping/views.py
+++ mapping/views.py
@@ -13,3 +13,10 @@ def index(request):
 	}
 
 	return render(request, 'index.html', context=context)
+def mapRender(request):
+	mapbox_access_token = 'pk.eyJ1IjoidGhhZGlzaGkiLCJhIjoiY2prdGVpZ282MDU5bDNrbGplZjdkajcwMyJ9.6j75DNDrM9M_76hSPBZryQ'
+
+	
+	return render(request, 'mapRender.html', {
+		'mapbox_access_token': mapbox_access_token
+	})
