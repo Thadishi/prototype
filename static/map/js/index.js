3669351737b96ff2174f3cb8e19386e5f77617ce
Riyaadh <riyaadh.abr@gmail.com>
2018-08-16 10:07:41 +0200

    got it working with the data

--- static/map/js/index.js
+++ static/map/js/index.js
@@ -1,50 +1,21 @@
-const data = [
-  { pickup: [17.99219,-32.973039], dropoff: [27.955786,-26.010472], tripStartTime: 5, tripEndTime: 0 }
-]
-
 const MALE_COLOR = [0, 128, 255];
 const FEMALE_COLOR = [255, 0, 128];
 
-//var tData = JSON.parse("data/highRisk.json")
-
-/*var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-        alert(myObj.name);
-    }
-};
-xmlhttp.open("GET", "data/highRisk.json", true);
-xmlhttp.send();*/ 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiZXpla2llbGNoZW50bmlrIiwiYSI6ImNqOWZ3b2JpMTJxc2cyd3FtemJlZXUwczYifQ.2hnqN-NsAxJn_td8dWWA2w'
-
-const INITIAL_VIEW_STATE = {
-  latitude: 30.5,
-  longitude: 23,
-  zoom: 3
-};
-
-new DeckGL({
-  container: document.getElementById("container"),
-  // mapbox: false, /* disable map */
+new deck.DeckGL({
+  mapboxApiAccessToken: '',
+  mapStyle: 'https://free.tilehosting.com/styles/positron/style.json?key=U0iNgiZKlYdwvgs9UPm1',
   longitude: 20,
   latitude: -33.5,
   zoom: 7,
-  pitch: 10,
-  layers: [new DeckGL({
-    mapboxApiAccessToken: '<your_token_here>',
-    mapStyle: 'mapbox://styles/mapbox/light-v9',
-    longitude: 20,
-    latitude: -33.5,
-    zoom: 7,
-    layers: [
-      new DeckGL.ScatterplotLayer({
-
-        
-      })
-    ]
-  })
-
+  maxZoom: 16,
+  layers: [
+    new deck.ScatterplotLayer({
+      id: 'scatter-plot',
+      data: 'https://raw.githubusercontent.com/ThaboKopane/prototype/master/static/data/data.json',
+      radiusScale: 10,
+      radiusMinPixels: 0.5,
+      getPosition: d => [d[0], d[1], 0],
+      //getColor: d => (d[2] === 1 ? MALE_COLOR : FEMALE_COLOR)
+    })
   ]
-})
+});
